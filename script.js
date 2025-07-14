28b33caa1327d2496caf1253844b6d7b71f42970
Nils Alberding-Schulz <nilsalberding@web.de>
2025-07-14 10:06:09 +0200

    add function showCart() for mobile cartview

---
+++ script.js
@@ -0,0 +1,27 @@
+// Produkte rendern
+
+    // Klasse für Produkte erstellen
+
+    // Database für Produkte erstellen
+
+// Warenkorb-items hinzufügen
+
+// Warenkorb-Items Anzahl verändern
+
+    // Anzahl erhöhen
+
+    // Anzahl verringern
+
+    // Item komplett löschen
+
+// Warenkorb responsive anzeigen
+
+
+function showCart(){
+
+    const cartRef = document.getElementById('cart');
+
+    cartRef.classList.toggle('d-none');
+}
+
+// 
